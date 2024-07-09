e6e68805ffb9d82da23b84b235cfd1abf9ed7e4a
Andrei Starichenko <centroid@mail.ru>
2024-07-09 11:46:28 +0300

    apply last pypi template

--- explore.py
+++ explore.py
@@ -1,8 +1,2 @@
 # DON'T DELETE!
 # useful to start smth without pytest and not to run in main script!
-
-from cli_user import *
-
-victim = CliUser()
-victim.send("ping localhost", timeout=0.1)
-victim.print_state()
